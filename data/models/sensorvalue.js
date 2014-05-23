bc7e4799408c9075a6df7500d318ab4903b949b2
micwolljung <michael.wolljung@wanadoo.fr>
2014-05-23 17:29:07 +0200

    added database tables and functions

--- data/models/sensorvalue.js
+++ data/models/sensorvalue.js
@@ -52,7 +52,113 @@ function saveValue(thing,cb){
 	});
 }
 
+function loadTriggers(cb){
+	var db;
+	var triggers = [];
+	db = new sqlite3.Database('./data/db.db');
+	db.serialize(function(){
+		db.each("SELECT * FROM Triggers", function(err, row) {
+      		triggers.push(row);
+  		},function(err){
+  			db.close();
+  			cb(err,triggers);
+  		});
+	});
+}
+
+function loadRules(cb){
+	var db;
+	var rules = [];
+	db = new sqlite3.Database('./data/db.db');
+	db.serialize(function(){
+		db.each("SELECT * FROM Rules", function(err, row) {
+      		rules.push(row);
+  		},function(err){
+  			db.close();
+  			cb(err,rules);
+  		});
+	});
+}
+
+function loadActions(cb){
+	var db;
+	var actions = [];
+	db = new sqlite3.Database('./data/db.db');
+	db.serialize(function(){
+		db.each("SELECT * FROM Actions", function(err, row) {
+      		actions.push(row);
+  		},function(err){
+  			db.close();
+  			cb(err,actions);
+  		});
+	});
+}
+
+function saveRule(triggerId, actionId, cb){
+	var db;
+	db = new sqlite3.Database('./data/db.db');
+	db.serialize(function(){
+		var stmt = db.run("INSERT INTO Rules VALUES (?,?);",[triggerId,actionId],function(err){
+			db.close();
+			cb(err);
+		});
+	});
+}
+
+function saveTrigger(sensorId, operator, threshold, cb){
+	var db;
+	db = new sqlite3.Database('./data/db.db');
+	db.serialize(function(){
+		var stmt = db.run("INSERT INTO Triggers VALUES (?,?,?);",[sensorId,operator,threshold],function(err){
+			db.close();
+			cb(err);
+		});
+	});
+}
+
+function deleteRule(ruleId, cb){
+	var db;
+	db = new sqlite3.Database('./data/db.db');
+	db.serialize(function(){
+		var stmt = db.run("DELETE FROM Rules WHERE id=(?);",[ruleId],function(err){
+			db.close();
+			cb(err);
+		});
+	});
+}
+
+
+function deleteTrigger(triggerId, cb){
+	var db;
+	db = new sqlite3.Database('./data/db.db');
+	var stmt = db.get("SELECT * FROM Rules WHERE triggerId=(?);",[triggerId],function(err, row) {
+	   	if (row!=undefined) {
+	     	db.close();
+			cb(err);
+		}
+		else {
+			db.serialize(function(){
+				stmt = db.run("DELETE FROM Triggers WHERE id=(?);",[triggerId],function(err){
+					db.close();
+					cb(err);
+				});
+			});
+		}
+  	},function(err){
+		db.close();
+		cb(err);
+	});
+}
+
+
 module.exports.loadSensors = loadSensors;
 module.exports.loadSensor = loadSensor;
 module.exports.saveValue = saveValue;
 module.exports.listSensors = listSensors;
+module.exports.loadTriggers = loadTriggers;
+module.exports.loadRules = loadRules;
+module.exports.loadActions = loadActions;
+module.exports.saveRule = saveRule;
+module.exports.saveTrigger = saveTrigger;
+module.exports.deleteRule = deleteRule;
+module.exports.deleteTrigger = deleteTrigger;
--- db_init.js
+++ db_init.js
@@ -4,6 +4,9 @@ var db = new sqlite3.Database('./data/db.db');
 db.serialize(function(){
 	db.run("CREATE TABLE Things (id TEXT PRIMARY KEY, type TEXT, subType TEXT)");
 	db.run("CREATE TABLE SensorValues (thingId TEXT, date INT, numericValue INT, textValue TEXT)");
+	db.run("CREATE TABLE Triggers (id INT PRIMARY KEY AUTOINCREMENT, sensorId TEXT, operator TEXT, threshold INT)");
+	db.run("CREATE TABLE Actions (id INT PRIMARY KEY AUTOINCREMENT, actuatorId TEXT, value TEXT)");
+	db.run("CREATE TABLE Rules (id INT PRIMARY KEY AUTOINCREMENT, triggerId INT, actionId INT)");
 });
 
 db.close();
